27736776ae238ff0a1ababd22bc516b3953ce667
Caio Amaral <cbarreto_sud@hotmail.com>
2020-04-02 16:08:12 -0300

    [web] Created new user and client functions

--- web/src/pages/NewClient/index.js
+++ web/src/pages/NewClient/index.js
@@ -1,26 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import api from '../../services/api';
+import { history } from '../../routes';
+
 export default function NewClient() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [address, setAddress] = useState('');
+    const [phone, setPhone] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        api.post('/client', {
+            name,
+            email,
+            address,
+            phone
+        }).then(() => {
+            history.push('/clients');
+        }).catch(error => console.log(error));
+    }
+
     return (
         <div className="container max-w-screen-lg">
             <h1>New Client</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="block sm:flex items-center">
-                    <label htmlFor="username" className="sm:mr-3">Name:</label>
-                    <input id="username" className="mt-3 sm:mt-0" required />
+                    <label htmlFor="name" className="sm:mr-3">Name:</label>
+                    <input
+                        id="name"
+                        className="mt-3 sm:mt-0"
+                        value={name}
+                        onChange={(event) => setName(event.target.value)}
+                        required
+                    />
                 </div>
                 <div className="block sm:flex items-center">
                     <label htmlFor="email" className="sm:mr-3">CPF:</label>
-                    <input id="email" className="mt-3 sm:mt-0" required />
+                    <input
+                        id="email"
+                        className="mt-3 sm:mt-0"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                        required
+                    />
                 </div>
                 <div className="block sm:flex items-center">
                     <label htmlFor="address" className="sm:mr-3">Address:</label>
-                    <input id="address" className="mt-3 sm:mt-0" required />
+                    <input
+                        id="address"
+                        className="mt-3 sm:mt-0"
+                        value={address}
+                        onChange={(event) => setAddress(event.target.value)}
+                        required
+                    />
                 </div>
                 <div className="block sm:flex items-center">
                     <label htmlFor="phone" className="sm:mr-3">Phone:</label>
-                    <input id="phone" className="mt-3 sm:mt-0" required />
+                    <input
+                        id="phone"
+                        className="mt-3 sm:mt-0"
+                        value={phone}
+                        onChange={(event) => setPhone(event.target.value)}
+                        required
+                    />
                 </div>
                 <div className="text-center">
                     <Link className="bg-red-500 button" to="/clients">Back</Link>
--- web/src/pages/NewUser/index.js
+++ web/src/pages/NewUser/index.js
@@ -1,26 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import { history } from '../../routes';
+import api from '../../services/api';
+
 export default function NewUser() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        api.post('/user', {
+            username,
+            email,
+            phone,
+            password
+        }).then(() => {
+            history.push('/users');
+        }).catch(error => console.log(error));
+    }
+
     return (
         <div className="container max-w-screen-lg">
             <h1>New User</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="block sm:flex items-center">
                     <label htmlFor="username" className="sm:mr-3">Username:</label>
-                    <input id="username" className="mt-3 sm:mt-0" required />
+                    <input
+                        id="username"
+                        className="mt-3 sm:mt-0"
+                        value={username}
+                        onChange={(event) => setUsername(event.target.value)}
+                        required
+                    />
                 </div>
                 <div className="block sm:flex items-center">
                     <label htmlFor="email" className="sm:mr-3">Email:</label>
-                    <input id="email" className="mt-3 sm:mt-0" required type="mail" />
+                    <input
+                        id="email"
+                        className="mt-3 sm:mt-0"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                        type="mail"
+                        required
+                    />
                 </div>
                 <div className="block sm:flex items-center">
                     <label htmlFor="phone" className="sm:mr-3">Phone:</label>
-                    <input id="phone" className="mt-3 sm:mt-0" />
+                    <input
+                        id="phone"
+                        className="mt-3 sm:mt-0"
+                        value={phone}
+                        onChange={(event) => setPhone(event.target.value)}
+                    />
                 </div>
                 <div className="block sm:flex items-center">
                     <label htmlFor="password" className="sm:mr-3">Password:</label>
-                    <input id="password" className="mt-3 sm:mt-0" required type="password" />
+                    <input
+                        id="password"
+                        className="mt-3 sm:mt-0"
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                        type="password"
+                        required
+                    />
                 </div>
                 <div className="text-center">
                     <Link className="bg-red-500 button" to="/users">Back</Link>
